b2be51008536be3a376586ed193d6ced977b6843
Yusuf Boyacı <85557228+yusufboyaci@users.noreply.github.com>
2022-02-03 20:27:41 +0300

    identity kısmı veri tabanına göre ayarlandı.

--- UI/Controllers/AccountController.cs
+++ UI/Controllers/AccountController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Authentication;
+﻿using DATAACCESS.Abstract;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Mvc;
 using System.Security.Claims;
 using UI.Models;
@@ -7,13 +8,20 @@ namespace UI.Controllers
 {
     public class AccountController : Controller
     {
+        private readonly IUserRepository _userRepository;
+        public AccountController(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
         public IActionResult Index()
         {
             return View();
         }
+      
         private bool LoginUser(string username, string password)
         {
-            if (username == "admin" && password == "123")
+            var user = _userRepository.GetAll().FirstOrDefault(x => x.Username == username && x.Password == password);
+            if (user != null)
             {
                 return true;
             }
