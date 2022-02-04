3c7e8f081b4789bbe6c47aea2cacb4bcdfcae56c
Yusuf Boyacı <85557228+yusufboyaci@users.noreply.github.com>
2022-02-04 16:23:57 +0300

    Searching bar eklendi

--- UI/wwwroot/js/site.js
+++ UI/wwwroot/js/site.js
@@ -2,3 +2,23 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+$(document).ready(function () {
+
+    //Arama çubuğu yapma kodları User Index sayfasındaki tablonun üzerine eklenen input(arama çubuğu) ile çalışır.
+    $("#searchBar").keyup(function () {
+        let value = $(this).val().toLowerCase();
+        $("#userTable tr").filter(function () {
+            $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+        });
+    });
+
+   
+
+
+
+
+
+
+
+});
